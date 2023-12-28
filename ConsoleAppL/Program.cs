46388197bc6c68119e96b90489a6a3cb3c6bf510
LEX100500 <AleX69.galkin@yandex.ru>
2023-12-28 20:42:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppL/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppL
+{
+    internal class Program
+    {   
+        class Palindrom
+        {
+            int number;
+            int numberDigitsNum;
+            
+
+            public int Number { get => number; set => number = value; }
+            public int NumberDigitsNum { get => numberDigitsNum; set => numberDigitsNum = value; }
+
+            public Palindrom(int number,int numberDigitsNum)
+            {
+                this.number = number;
+                this.numberDigitsNum = numberDigitsNum;
+            }
+        }
+
+        private static bool isPalindrome(int Num)
+        {
+            string number = Convert.ToString(Math.Abs(Num));
+
+            if (number.Length == 0) 
+            {
+                return false;
+
+            }
+
+            for(int i =0;i<number.Length;i++)
+            {
+                if (number[i] != number[number.Length-i-1]) 
+                {
+                    return false;
+
+                }
+
+                if (i > number.Length - 1)
+                {
+                    return true;
+                }
+            }
+            return true;
+
+        }
+
+        private static int PalendromoDigitsMult(int Num)
+        {
+            int mult = 1;
+            string number = Convert.ToString(Math.Abs(Num));
+
+            for(int i=0;i<number.Length;i++)
+            {
+                if(i> number.Length -i - 1)
+                {
+                    break;
+                }
+                if (number[i] != '0')
+                {
+                    // Если ошибка то + convert to string
+                    mult *= int.Parse(Convert.ToString(number[i]*number[i]));
+                }
+            }
+
+            return mult;
+        }
+
+        private static Dictionary<int, int> GroupByValues(List<int>numbers)
+        {
+            Dictionary<int,int> keyValuePairs= new Dictionary<int,int>();
+            foreach(int number in numbers)
+            {
+                if (!keyValuePairs.ContainsKey(number))
+                {
+                    keyValuePairs.Add(number, 1);
+                }
+                else continue;
+
+
+                foreach(int num in numbers)
+                {
+                    if (number == num)
+                    {
+                        keyValuePairs[number]++;
+                    }
+                }
+            }
+            return keyValuePairs;
+        }
+
+        static void Main()
+        {
+            List<Palindrom> palendroms = new List<Palindrom>();
+            List<int> multOfPalendroms = new List<int>();
+            for(int i=100;i<1000000000;i++)
+            {
+                if (isPalindrome(i))
+                {
+                    palendroms.Add(new Palindrom(i,PalendromoDigitsMult(i)));
+                    multOfPalendroms.Add((PalendromoDigitsMult(i)));
+                }
+
+                PalendromoDigitsMult(i);
+
+
+            }
+            
+        }
+    }
+}
---
+++ ConsoleAppL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleAppL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppL")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7e8e9c00-012d-43ca-befa-4a65ded12463")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
